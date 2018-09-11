2239d1112412d8e601bdce4927561b18cd3c9033
mcurrr <technosigal@gmail.com>
2018-09-11 21:56:32 +0300

    main to ES6

--- js/main.js
+++ js/main.js
@@ -10,269 +10,272 @@ const styleForAllText = new window.PIXI.TextStyle({
 });
 
 //---------------GAME ------------------//
-let Game = function() {
-    this._score = 0;
-    this._superScore = 0;
-    this._allEnemies = [];
-    this._intervalEnemy;
-    this._intervalSuperPower;
-    this._superAimCount = 0;
+class Game {
+    constructor(game, speed) {
+        this._score = 0;
+        this._superScore = 0;
+        this._allEnemies = [];
+        this._intervalEnemy;
+        this._intervalSuperPower;
+        this._superAimCount = 0;
 
-    this.initApp();
-    this.buttonPlay(WIDTH / 2, HEIGHT / 2);
-};
-
-Game.prototype.buttonPlay = function(x, y) {
-    //TODO settings
-    // const gameplay = new Gameplay(this);
-    const buttonStart = createSprite(this.app, {
-        x: x,
-        y: y,
-        width: 200,
-        height: 200,
-        path: 'images/interface/button-play.png',
-        interactive: true
-    });
-
-    buttonStart.on("click", () => {
-        this.app.stage.removeChild(buttonStart);
-        // gameplay.destroyIcon();
-
-        this.addPlayer(WIDTH / 2, HEIGHT / 2);
-        this.drawAim(WIDTH / 2, HEIGHT / 2);
-        this.playerScore();
-        this.createHeart();
-        initAnimation();
-        this._intervalEnemy = setInterval(() => {
-            const enemy = Enemy.getRandomEnemy(this);
-            this._allEnemies.push(enemy);
-        }, 1500);
-
-        this._onMouseMove = this._onMouseMove.bind(this);
-        this._playerShot = this._playerShot.bind(this);
-
-        setTimeout(() => {
-            document.addEventListener('mousemove', this._onMouseMove);
-            document.addEventListener('click', this._playerShot);
-        }, 0);
-    });
-};
-
-Game.prototype._playerShot = function(event) {
-    let newShot = new Shot(event.clientX, event.clientY, this);
-};
-
-Game.prototype._onMouseMove = function(event) {
-    const self = this;
-    const playerCenter = {
-        x: self._player.x,
-        y: self._player.y
-    };
-
-    const angle = Math.atan2(event.clientX - playerCenter.x, -(event.clientY - playerCenter.y)) * (180 / Math.PI);
-
-    self.rotatePlayer(angle);
-    self._aim.x = event.clientX;
-    self._aim.y = event.clientY;
-}
-
-Game.prototype.superPower = function() {
-    const self = this;
-    let x = 0,
-        y = 0;
-
-    document.addEventListener('mousemove', function(event) {
-        x = event.clientX;
-        y = event.clientY;
-    });
-
-
-    self._intervalSuperPower = setInterval(function(event) {
-        let newShot = new Shot(x, y, self);
-    }, 100);
-
-    setTimeout(function() {
-        clearInterval(self._intervalSuperPower);
-    }, 7000);
-};
-
-Game.prototype.stopInterval = function() {
-    clearInterval(this._intervalEnemy);
-    clearInterval(this._intervalSuperPower);
-};
-
-Game.prototype.initApp = function() {
-    const self = this;
-    self.app = new PIXI.Application(WIDTH, HEIGHT, { backgroundColor: 0x1099bb });
-    document.body.appendChild(self.app.view);
-};
-
-Game.prototype.drawAim = function(x, y) {
-    this._aim = createSprite(this.app, {
-        x: x,
-        y: y,
-        width: 128,
-        height: 128,
-        path: 'images/aim.png'
-    });
-}
-
-Game.prototype.addPlayer = function(x, y) {
-    this._player = createSprite(this.app, {
-        path: 'images/player.png',
-        x: x,
-        y: y,
-        width: 140,
-        height: 95
-    });
-};
-
-Game.prototype.rotatePlayer = function(deg) {
-    this._player.rotation = inRad(deg);
-};
-
-Game.prototype._endGame = function() {
-    document.removeEventListener('mousemove', this._onMouseMove);
-    document.removeEventListener('click', this._playerShot);
-    this.stopInterval();
-    this._allEnemies.forEach(function(el) {
-        el.destroy();
-    });
-
-    this._player.destroy();
-    this._aim.destroy();
-    this.showInfo();
-    this.restartGame();
-};
-
-Game.prototype.showInfo = function() {
-    const infoText = "Your high score : ";
-    const playerHightScore = new PIXI.Text(infoText + this._score, styleForAllText);
-
-    this._scoreTicker.stop();
-    this._scoreTicker.destroy();
-    this._playerScoreText.destroy();
-
-    playerHightScore.x = WIDTH / 2;
-    playerHightScore.y = HEIGHT / 2;
-    playerHightScore.anchor.set(0.5);
-    setScale(playerHightScore);
-
-    this.app.stage.addChild(playerHightScore);
-};
-
-Game.prototype.playerScore = function() {
-    const xAndY = percentages(3, 3);
-    const ticker = new window.PIXI.ticker.Ticker();
-    let playerScoreText = new PIXI.Text(this._score, styleForAllText);
-
-    ticker.stop();
-    ticker.add(() => {
-        this.app.stage.removeChild(playerScoreText);
-        playerScoreText = new PIXI.Text(this._score, styleForAllText);
-
-        playerScoreText.x = xAndY.x;
-        playerScoreText.y = xAndY.y;
-
-        this.app.stage.addChild(playerScoreText);
-
-    });
-
-    this._playerScoreText = playerScoreText;
-    this._scoreTicker = ticker;
-    ticker.start();
-};
-
-Game.prototype.decreaseScore = function() {
-    let lastElement = this._scoreHearts.pop();
-
-    if (lastElement) {
-        lastElement.destroy();
+        this.initApp();
+        this.buttonPlay(WIDTH / 2, HEIGHT / 2);
     }
 
-    if (this._scoreHearts.length <= 0) {
-        this._endGame();
-    }
-};
-
-Game.prototype.createHeart = function() {
-    this._scoreHearts = [];
-    const xAndY = percentages(65, 7);
-    let stepX = xAndY.x;
-    for (let i = 0; i <= 2; i++) {
-
-        const heart = createSprite(this.app, {
-            x: stepX,
-            y: xAndY.y,
-            width: 100,
-            height: 100,
-            path: 'images/interface/heart-live.png'
+    buttonPlay (x, y) {
+        //TODO settings
+        // const gameplay = new Gameplay(this);
+        const buttonStart = createSprite(this.app, {
+            x: x,
+            y: y,
+            width: 200,
+            height: 200,
+            path: 'images/interface/button-play.png',
+            interactive: true
         });
 
-        stepX += heart.width + heart.width / 6;
+        buttonStart.on("click", () => {
+            this.app.stage.removeChild(buttonStart);
+            // gameplay.destroyIcon();
 
-        this._scoreHearts.push(heart);
-    };
-};
+            this.addPlayer(WIDTH / 2, HEIGHT / 2);
+            this.drawAim(WIDTH / 2, HEIGHT / 2);
+            this.playerScore();
+            this.createHeart();
+            initAnimation();
+            this._intervalEnemy = setInterval(() => {
+                const enemy = Enemy.getRandomEnemy(this);
+                this._allEnemies.push(enemy);
+            }, 1500);
 
-//CONTROL SUPER POWER
-/*
-this._superScore - unstoped shot
-this._superAimCount - auto aim
-*/
-Game.prototype.hitEnemy = function() {
-    this._score += 100;
-    this._superScore += 100;
-    this._superAimCount += 1;
+            this._onMouseMove = this._onMouseMove.bind(this);
+            this._playerShot = this._playerShot.bind(this);
 
-    if (this._superScore === 5000) {
-        this.superPower();
-        this._superScore = 0;
+            setTimeout(() => {
+                document.addEventListener('mousemove', this._onMouseMove);
+                document.addEventListener('click', this._playerShot);
+            }, 0);
+        });
     }
 
-    if (this._superAimCount === 30) {
-        this.superAim();
-        this._superAimCount = 0;
+    _playerShot(event) {
+        let newShot = new Shot(event.clientX, event.clientY, this);
     }
-};
 
-Game.prototype.superAim = function() {
-    let allEnemies = this._allEnemies;
+    _onMouseMove(event) {
+        const self = this;
+        const playerCenter = {
+            x: self._player.x,
+            y: self._player.y
+        };
 
-    const superKill = setInterval(function() {
-        allEnemies.forEach((en) => {
-            document.dispatchEvent(new MouseEvent('click', {
-                bubbles: true,
-                cancelable: true,
-                view: window,
-                clientX: en.x,
-                clientY: en.y
-            }));
-        })
+        const angle = Math.atan2(event.clientX - playerCenter.x, -(event.clientY - playerCenter.y)) * (180 / Math.PI);
 
-    }, 300);
+        self.rotatePlayer(angle);
+        self._aim.x = event.clientX;
+        self._aim.y = event.clientY;
+    }
 
-    setTimeout(function() {
-        clearInterval(superKill);
-    }, 7000);
-};
+    superPower() {
+        const self = this;
+        let x = 0,
+            y = 0;
 
-Game.prototype.restartGame = function() {
-    const self = this;
-    const restart = createSprite(this.app, {
-        x: WIDTH / 2,
-        y: HEIGHT / 3,
-        width: 200,
-        height: 200,
-        path: 'images/interface/restart.png',
-        interactive: true
-    });
+        document.addEventListener('mousemove', function(event) {
+            x = event.clientX;
+            y = event.clientY;
+        });
 
-    //TODO - delete eventListenner All
-    restart.on('click', function() {
-        document.body.removeChild(self.app.view);
-        let game = new Game();
-    });
-};
+
+        self._intervalSuperPower = setInterval(function(event) {
+            let newShot = new Shot(x, y, self);
+        }, 100);
+
+        setTimeout(function() {
+            clearInterval(self._intervalSuperPower);
+        }, 7000);
+    }
+
+    stopInterval() {
+        clearInterval(this._intervalEnemy);
+        clearInterval(this._intervalSuperPower);
+    }
+
+    initApp () {
+        const self = this;
+        self.app = new PIXI.Application(WIDTH, HEIGHT, { backgroundColor: 0x1099bb });
+        document.body.appendChild(self.app.view);
+    }
+
+    drawAim(x, y) {
+        this._aim = createSprite(this.app, {
+            x: x,
+            y: y,
+            width: 128,
+            height: 128,
+            path: 'images/aim.png'
+        });
+    }
+
+    addPlayer(x, y) {
+        this._player = createSprite(this.app, {
+            path: 'images/player.png',
+            x: x,
+            y: y,
+            width: 140,
+            height: 95
+        });
+    }
+
+    rotatePlayer(deg) {
+        this._player.rotation = inRad(deg);
+    }
+
+    _endGame() {
+        document.removeEventListener('mousemove', this._onMouseMove);
+        document.removeEventListener('click', this._playerShot);
+        this.stopInterval();
+        this._allEnemies.forEach(function(el) {
+            el.destroy();
+        });
+
+        this._player.destroy();
+        this._aim.destroy();
+        this.showInfo();
+        this.restartGame();
+    }
+
+    showInfo() {
+        const infoText = "Your high score : ";
+        const playerHightScore = new PIXI.Text(infoText + this._score, styleForAllText);
+
+        this._scoreTicker.stop();
+        this._scoreTicker.destroy();
+        this._playerScoreText.destroy();
+
+        playerHightScore.x = WIDTH / 2;
+        playerHightScore.y = HEIGHT / 2;
+        playerHightScore.anchor.set(0.5);
+        setScale(playerHightScore);
+
+        this.app.stage.addChild(playerHightScore);
+    }
+
+    playerScore () {
+        const xAndY = percentages(3, 3);
+        const ticker = new window.PIXI.ticker.Ticker();
+        let playerScoreText = new PIXI.Text(this._score, styleForAllText);
+
+        ticker.stop();
+        ticker.add(() => {
+            this.app.stage.removeChild(playerScoreText);
+            playerScoreText = new PIXI.Text(this._score, styleForAllText);
+
+            playerScoreText.x = xAndY.x;
+            playerScoreText.y = xAndY.y;
+
+            this.app.stage.addChild(playerScoreText);
+
+        });
+
+        this._playerScoreText = playerScoreText;
+        this._scoreTicker = ticker;
+        ticker.start();
+    }
+
+    decreaseScore() {
+        let lastElement = this._scoreHearts.pop();
+
+        if (lastElement) {
+            lastElement.destroy();
+        }
+
+        if (this._scoreHearts.length <= 0) {
+            this._endGame();
+        }
+    }
+
+    createHeart() {
+        this._scoreHearts = [];
+        const xAndY = percentages(65, 7);
+        let stepX = xAndY.x;
+        for (let i = 0; i <= 2; i++) {
+
+            const heart = createSprite(this.app, {
+                x: stepX,
+                y: xAndY.y,
+                width: 100,
+                height: 100,
+                path: 'images/interface/heart-live.png'
+            });
+
+            stepX += heart.width + heart.width / 6;
+
+            this._scoreHearts.push(heart);
+        };
+    }
+
+    //CONTROL SUPER POWER
+    /*
+    this._superScore - unstoped shot
+    this._superAimCount - auto aim
+    */
+    hitEnemy () {
+        this._score += 100;
+        this._superScore += 100;
+        this._superAimCount += 1;
+
+        if (this._superScore === 5000) {
+            this.superPower();
+            this._superScore = 0;
+        }
+
+        if (this._superAimCount === 30) {
+            this.superAim();
+            this._superAimCount = 0;
+        }
+    }
+
+    superAim() {
+        let allEnemies = this._allEnemies;
+
+        const superKill = setInterval(function() {
+            allEnemies.forEach((en) => {
+                document.dispatchEvent(new MouseEvent('click', {
+                    bubbles: true,
+                    cancelable: true,
+                    view: window,
+                    clientX: en.x,
+                    clientY: en.y
+                }));
+            })
+
+        }, 300);
+
+        setTimeout(function() {
+            clearInterval(superKill);
+        }, 7000);
+    }
+
+    restartGame() {
+        const self = this;
+        const restart = createSprite(this.app, {
+            x: WIDTH / 2,
+            y: HEIGHT / 3,
+            width: 200,
+            height: 200,
+            path: 'images/interface/restart.png',
+            interactive: true
+        });
+
+        //TODO - delete eventListenner All
+        restart.on('click', function() {
+            document.body.removeChild(self.app.view);
+            let game = new Game();
+        });
+    }
+
+}
 
 let newGame = new Game();
