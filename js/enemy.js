46bd5e1d33ffc344fefdbd29f24fdf6f90372489
Andrew <oreljs13@gmail.com>
2018-09-25 16:00:49 +0300

    add js file enemyTypes

--- js/enemy.js
+++ js/enemy.js
@@ -111,43 +111,4 @@ class Enemy {
         Enemy.enemyInstances = Enemy.enemyInstances || [];
         Enemy.enemyInstances.push(creator);
     }
-
-
-    //static enemyInstances = []
 }
-
-class EnemyFast extends Enemy {
-    constructor(game) {
-        super(game, 100);
-        this._enemy._life = 1;
-    }
-}
-
-Enemy.registerNewEnemy(game => new EnemyFast(game));
-
-class EnemySlow extends Enemy {
-    constructor(game) {
-        super(game, 200);
-        this._enemy._life = 1;
-    }
-}
-
-Enemy.registerNewEnemy(game => new EnemySlow(game));
-
-class EnemySuperSlow extends Enemy {
-    constructor(game) {
-        super(game, 300);
-        this._enemy._life = 1;
-    }
-}
-
-Enemy.registerNewEnemy(game => new EnemySuperSlow(game));
-
-class EnemyFourLife extends Enemy {
-    constructor(game) {
-        super(game, 300);
-        this._enemy._life = 2;
-    }
-}
-
-Enemy.registerNewEnemy(game => new EnemyFourLife(game));
